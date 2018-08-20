bcd25544ed9dade4683ac1a099a2cbc853b4b9e7
Rekai Musuka <musukarekai@gmail.com>
2018-08-20 16:21:15 +0100

    Completed Basic version of Users Controller and subsequent Views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,39 @@
 class UsersController < ApplicationController
-  def new
-  end
-
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user)
   end
 
   def create
+      # Untested Code
+    user = User.new(user_params)
+
+    if user.save
+      redirect_to user_path(user)
+    else
+      raise
+    end
   end
 
   def show
-  end
-
-  def index
+    @user = User.find(params[:id])
   end
 
   def destroy
+    user = User.find(params[:id])
+    user.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :first_name, :last_name, :description)
   end
 end
