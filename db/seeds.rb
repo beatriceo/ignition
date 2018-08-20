6c345ad41dfe6e0d54ec96d4bb3444a2294782b6
Beatrice Olivera <beatriceolivera1999@gmail.com>
2018-08-20 16:17:47 +0100

    created seed and finished index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Listing.destroy_all
+User.destroy_all
+
+listing = Listing.new(transportation_type: "car", cost: "£100", location: "london", date_start: '2018-08-20', date_end: '2018-08-21', rating: 3, description: "awesome")
+user = User.new(email: "test@test.com", username: "test", password: "test123", first_name: "test", last_name: "test", rating: 0, description: "shitty person")
+user.save
+listing.user = user
+listing.save
+
+puts "done!"
