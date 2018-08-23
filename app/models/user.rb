d62e4eda5b6123ddfa269166a84ef749a2f0f887
Beatrice Olivera <beatriceolivera1999@gmail.com>
2018-08-23 21:59:48 +0100

    added photo column to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
   has_many :offers, dependent: :destroy
   has_many :listings, dependent: :destroy
   validates :rating, inclusion: {in: (0..5).to_a}, numericality: {only_integer: true}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_23_093402) do
+ActiveRecord::Schema.define(version: 2018_08_23_205657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2018_08_23_093402) do
     t.integer "rating", default: 0
     t.text "description"
     t.boolean "admin", default: false
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
